972da237e3323848770982e962c31f0c102c0242
louis61619 <louis61619@gmail.com>
2024-09-11 14:20:42 +0800

    fix none package

--- src/materials/textarea/view.tsx
+++ src/materials/textarea/view.tsx
@@ -3,9 +3,8 @@ import { CompProps } from './type';
 import { Textarea } from '@mantine/core';
 import { useField } from '@mantine/form';
 import { CommonConfig, CommonInputWrapper } from '../common';
-import { CommonProps } from '@mui/material/OverridableComponent';
 
-export const View: ViewCompProps<CompProps & CommonProps> = ({ configValue, inputProps }) => {
+export const View: ViewCompProps<CompProps> = ({ configValue, inputProps }) => {
   return (
     <CommonInputWrapper {...configValue}>
       <Textarea placeholder={configValue.placeholder} rows={configValue.rows} {...inputProps} />
